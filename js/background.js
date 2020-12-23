7709957d33d14858456f528ba6ec78a57cbeea79
Smit Desai <57582187+smitdesai1010@users.noreply.github.com>
2020-12-23 13:33:04 -0500

    music player done

--- js/background.js
+++ js/background.js
@@ -3,7 +3,6 @@ alert('ok')
 chrome.runtime.onMessage.addListener( letsGo );
 
 function letsGo(data){
-    alert("ok"+data.url+data.id+data.name+"end")
-    console.log("console")
+    alert("ok"+data.url+data.id+data.name+"end")  
 }
 
