428759a9aa94c8bff77881b94df2c59f225460af
Smit Desai <57582187+smitdesai1010@users.noreply.github.com>
2021-05-11 18:48:57 -0400

    added developer details

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
-document.querySelectorAll("a")
+document.querySelectorAll(".primarybtn")
     .forEach(ele => ele.addEventListener( "click" , 
         () => chrome.tabs.query( {  active: true,  currentWindow: true } , 
             (tabs) => chrome.runtime.sendMessage( {url: tabs[0].url, tag: ele.id} ) 
 )));
+
+document.getElementById("contact")
+    .addEventListener('click', () => {
+        let profile = document.getElementById("profile")
+        profile.style.display = profile.style.display == "none" ? "block" : "none";    
+    })
 
